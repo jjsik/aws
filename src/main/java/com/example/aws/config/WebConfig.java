ab2d9242f22416d9375c218258ad3a2061bc32f3
jjsik <withfresh143@gmail.com>
2025-11-20 15:21:32 +0900

    update url

--- src/main/java/com/example/aws/config/WebConfig.java
+++ src/main/java/com/example/aws/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://dnta295gra8nl.cloudfront.net")
+                .allowedOrigins("http://localhost:3000", "http://climate-search-frontend.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
