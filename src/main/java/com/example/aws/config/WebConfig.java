bef99de506ac6564e70d06e7fd37c540ee1b26e4
jjsik <withfresh143@gmail.com>
2025-11-20 15:07:59 +0900

    update - webconfig ;

--- src/main/java/com/example/aws/config/WebConfig.java
+++ src/main/java/com/example/aws/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://dnta295gra8nl.cloudfront.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
